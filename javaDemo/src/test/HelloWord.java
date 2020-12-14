fef6afe9c5071c21d339fbd6d7e7ecab3628bd83
Tom <54508909@qq.com>
2020-12-14 14:31:26 +0800

    添加demo工程到git

---
+++ javaDemo/src/test/HelloWord.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 利哥
+ * @Date: 2020/12/14 14:29
+ * @Description:
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+    }
+}
