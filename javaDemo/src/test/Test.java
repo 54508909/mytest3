b13e6dea1360b78454d6fcd75b2bf6361d02f1e4
Tom <54508909@qq.com>
2020-12-14 21:54:29 +0800

    1111

--- javaDemo/src/test/Test.java
+++ javaDemo/src/test/Test.java
@@ -11,5 +11,10 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("2222222222");
+        System.out.println("3333333333333333");
+        System.out.println("22222222222222222");
+        System.out.println("1111111111111");
+        System.out.println(1);
+        System.out.println(999999999);
     }
 }
