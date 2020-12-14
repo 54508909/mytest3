3a5cedb34ad8afd1230b1f012aef6d6e65c3feb6
Tom <54508909@qq.com>
2020-12-14 14:08:25 +0800

    添加demo工程到git

---
+++ javaDemo/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 利哥
+ * @Date: 2020/12/13 20:16
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
